a2a3a19bdf113410092121c67fa642b5726b62f1
Pratik Saxena <pratik2709@gmail.com>
2016-07-24 20:05:54 +0530

    PS:modified drawable

--- galaxian.js
+++ galaxian.js
@@ -4,16 +4,11 @@ var imageRepository = new function(){
 }
 
 function Drawable(){
-    this.init = function(x,y){
+    this.init = function(x,y, width, height){
         this.x = x;
         this.y = y;
-    };
-    this.speed = 0;
-    this.canvasWidth = 0;
-    this.canvasHeight = 0;
-
-    this.draw = function(){
-
+        this.width = width;
+        this.height = height;
     };
 
 }
