0c31fac00ee7f758eccf95aff4e78387b2cbbada
Pratik Saxena <pratik2709@gmail.com>
2016-07-24 19:32:38 +0530

    PS:added other attributes and draw function

--- galaxian.js
+++ galaxian.js
@@ -4,5 +4,16 @@ var imageRepository = new function(){
 }
 
 function Drawable(){
+    this.init = function(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    this.speed = 0;
+    this.canvasWidth = 0;
+    this.canvasHeight = 0;
+
+    this.draw = function(){
+
+    };
 
 }
