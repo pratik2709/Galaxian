4368104e5b3aa30648a9dfc5fad6232f6384de81
Pratik Saxena <pratik2709@gmail.com>
2016-07-24 19:47:31 +0530

    PS:added panning background

--- galaxian.js
+++ galaxian.js
@@ -17,3 +17,19 @@ function Drawable(){
     };
 
 }
+
+function Background(){
+    this.speed = 1;
+    this.draw = function() {
+        this.y += this.speed;
+        this.context.drawImage(imageRepository.background, this.x, this.y);
+        this.context.drawImage(imageRepository.background, this.x, this.y - this.canvasHeight);
+
+        if (this.y >= this.canvasHeight) {
+            this.y = 0;
+        }
+
+    };
+}
+
+Background.prototype = new Drawable();
