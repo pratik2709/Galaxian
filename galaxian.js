5181651e551595a5f7d972d7c87c65ad564051d3
Pratik Saxena <pratik2709@gmail.com>
2016-07-24 19:57:02 +0530

    PS:added panning background final with assets and initializing game

--- galaxian.js
+++ galaxian.js
@@ -1,13 +1,13 @@
 var imageRepository = new function(){
     this.background = new Image();
-    this.background.src = "imgs/background.png"
+    this.background.src = "imgs/bg.png"
 }
 
 function Drawable(){
     this.init = function(x,y){
         this.x = x;
         this.y = y;
-    }
+    };
     this.speed = 0;
     this.canvasWidth = 0;
     this.canvasHeight = 0;
@@ -33,3 +33,50 @@ function Background(){
 }
 
 Background.prototype = new Drawable();
+
+
+function Game() {
+
+	this.init = function() {
+		this.bgCanvas = document.getElementById('background');
+		if (this.bgCanvas.getContext) {
+			this.bgContext = this.bgCanvas.getContext('2d');
+
+			Background.prototype.context = this.bgContext;
+			Background.prototype.canvasWidth = this.bgCanvas.width;
+			Background.prototype.canvasHeight = this.bgCanvas.height;
+
+			this.background = new Background();
+			this.background.init(0,0); // Set draw point to 0,0
+			return true;
+		} else {
+			return false;
+		}
+	};
+	// Start the animation loop
+	this.start = function() {
+		animate();
+	};
+}
+
+function animate() {
+	requestAnimFrame( animate );
+	game.background.draw();
+}
+
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame   ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
+
+var game = new Game();
+function init() {
+	if(game.init())
+		game.start();
+}
