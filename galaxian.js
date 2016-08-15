b92bc95753360d59c1dc030aea62c93050e0f5b8
Pratik Saxena <pratik2709@gmail.com>
2016-08-15 12:13:21 +0530

    PSK: modified drawable class

--- galaxian.js
+++ galaxian.js
@@ -41,6 +41,21 @@ function Drawable(){
         this.width = width;
         this.height = height;
     };
+    this.speed = 0;
+    this.canvasWidth = 0;
+    this.canvasHeight = 0;
+    this.collidableWith = "";
+    this.isColliding = false;
+    this.type = "";
+
+    this.draw = function(){
+    };
+    this.move = function(){
+    };
+
+    this.isCollidableWith = function(objects){
+        return (this.collidableWith === this.type);
+    };
 
 }
 
